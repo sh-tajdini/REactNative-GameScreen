a8036af29ba3e1418bcc4bcec0ff5b8ce2d71ad8
Shervin Tajdini <shervin.taj@gmail.com>
2023-10-04 14:49:35 -0400

    improving the buttons

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -6,8 +6,10 @@ function PrimaryButton({children}){
     }
     return (
         <View style={styles.buttonOuterContainer}> 
-        <Pressable style={({pressed}) => pressed ? [styles.buttonInnerContainer,styles.pressed]  
-        : styles.buttonInnerContainer} 
+        <Pressable style={({pressed}) => pressed ? 
+        [styles.buttonInnerContainer,styles.pressed]  
+        : styles.buttonInnerContainer
+    } 
         onPress={pressHandler} 
         android_ripple={{color:'#640233'}}
         >
