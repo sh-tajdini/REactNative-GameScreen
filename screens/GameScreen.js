0f7d5d92b5ed19ac3b4b160e705e4fa839721639
Shervin Tajdini <shervin.taj@gmail.com>
2023-10-05 11:47:44 -0400

    creating a title component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,12 @@
 import { View,Text,StyleSheet } from 'react-native';
+import Title from '../components/Title';
 function GameScreen(){
     return (
     <View style={styles.screen}>
-    <Text>Opponent's Guess</Text>
-    GUESS
+    <Title>
+        Opponent's Guess
+    </Title>
+    {/* GUESS */}
     <View>
     <Text>Higher or lower? </Text>
     {/* +
@@ -18,5 +21,6 @@ const styles = StyleSheet.create({
     screen:{
         flex:1,
         padding:24,
-    }
+    },
+   
 });
