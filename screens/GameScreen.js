92e89c76dc8484d80460cdc6ed2831924f60d02e
Shervin Tajdini <shervin.taj@gmail.com>
2023-10-05 11:21:29 -0400

    starting work on the game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,22 @@
-import { Text } from 'react-native';
+import { View,Text,StyleSheet } from 'react-native';
 function GameScreen(){
-    return <Text>Game Screen!</Text>
+    return (
+    <View style={styles.screen}>
+    <Text>Opponent's Guess</Text>
+    GUESS
+    <View>
+    <Text>Higher or lower? </Text>
+    {/* +
+    - */}
+    </View>
+    {/* <View>LOG ROUNDS</View> */}
+    </View>
+    );
 }
 export default GameScreen;
+const styles = StyleSheet.create({
+    screen:{
+        flex:1,
+        padding:12,
+    }
+});
