3ae9136ae11a70b45cae493565c3493ea62b6c5b
Shervin Tajdini <shervin.taj@gmail.com>
2023-10-05 09:50:07 -0400

    adding a background image

--- App.js
+++ App.js
@@ -5,7 +5,14 @@ import{LinearGradient} from 'expo-linear-gradient';
 export default function App() {
   return (
     <LinearGradient  colors={['#4e0329','#ddb52f']} style={styles.rootScreen}>
-      <StartGameScreen/>
+      <ImageBackground 
+      source={require('./assets/images/background.png')} 
+      resizeMode='cover'
+      style={styles.rootScreen}
+      imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen/>  
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -14,6 +21,8 @@ const styles = StyleSheet.create({
 
   rootScreen:{
     flex:1,
+  },
+  backgroundImage:{
+    opacity:0.15,
   }
-
 });
