37435b942d5a73f90c93253e64c4c620cc09e307
Shervin Tajdini <shervin.taj@gmail.com>
2023-10-11 12:30:29 -0400

    keyboard avoiding view

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { TextInput,View,StyleSheet,Alert,useWindowDimensions } from "react-native";
+import { TextInput,
+    View,StyleSheet,
+    Alert,
+    useWindowDimensions,
+    KeyboardAvoidingView,
+    ScrollView
+} from "react-native";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Colors from "../constants/colors";
 import Title from "../components/ui/Title";
@@ -29,7 +35,9 @@ function StartGameScreen({onPickNumber}){
 
     const marginTopDistance = height < 380 ? 30 : 100;
 return (
-    <View style={[styles.rootContainer,{marginTop:marginTopDistance}]}>
+    <ScrollView style={styles.screen}>
+    <KeyboardAvoidingView style={styles.screen} behavior="position">
+        <View style={[styles.rootContainer,{marginTop:marginTopDistance}]}>
         <Title>
             Guess My Number
         </Title>
@@ -56,6 +64,8 @@ return (
          </View>
     </Card>
      </View>
+     </KeyboardAvoidingView>
+     </ScrollView>
 
 ); 
 }
@@ -63,6 +73,9 @@ export default StartGameScreen;
 
 //const deviceHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
+    screen:{
+        flex:1,
+    },
     rootContainer:{
             flex:1,
             //marginTop:deviceHeight < 380  ? 30 : 100,
